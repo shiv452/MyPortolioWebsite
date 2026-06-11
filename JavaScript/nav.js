54a28a1d7d6ad122a68d66ad49266f4eb5665e69
Git_Project <shivamgoyal@gmail.com>
2026-06-11 22:47:17 +0530

    JS files added

---
+++ JavaScript/nav.js
@@ -0,0 +1,6 @@
+// ==========================================
+// nav.js - Navigation: hamburger menu + active link on scroll
+// openmenu() / closemenu() called from HTML onclick attributes
+// ===========================================
+
+var 
